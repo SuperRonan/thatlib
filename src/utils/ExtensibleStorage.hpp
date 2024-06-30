fc0cb3c5729fe402a8a256eefbf8fe2ee8625b19
Ronan <ro.cailleau@gmail.com>
2024-06-30 18:22:51 +0200

    [misc] Added the ExtensibleStorage<T>

---
+++ src/utils/ExtensibleStorage.hpp
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <vector>
+
+namespace that
+{
+	template <class T, class Alloc = std::allocator<T>>
+	class ExtensibleStorage
+	{
+	protected:
+
+		using IndexType = size_t;
+		std::vector<T, Alloc> _storage;
+
+	public:
+
+		constexpr ExtensibleStorage() = default;
+
+		constexpr ~ExtensibleStorage() = default;
+
+		constexpr ExtensibleStorage(ExtensibleStorage const& other) = default;
+
+		constexpr ExtensibleStorage(ExtensibleStorage&& other) noexcept = default;
+
+		constexpr ExtensibleStorage& operator=(ExtensibleStorage const& other) = default;
+
+		constexpr ExtensibleStorage& operator=(ExtensibleStorage&& other) noexcept = default;
+
+		void swap(ExtensibleStorage& other)
+		{
+			_storage.swap(other._storage);
+		}
+
+		void reserve(IndexType capacity)
+		{
+			_storage.reserve(capacity);
+		}
+
+		void reserveAdditionalIFN(IndexType extra_capacity)
+		{
+			const IndexType needed_capacity = size() + extra_capacity;
+			if (needed_capacity > capacity())
+			{
+				reserve(needed_capacity);
+			}
+		}
+
+		void resize(IndexType size)
+		{
+			_storage.resize(size);
+		}
+
+		void growIFN(IndexType needed)
+		{
+			_storage.resize(needed + _storage.size());
+		}
+
+		void shrink()
+		{
+			_storage.shrink_to_fit();
+		}
+
+		void clear()
+		{
+			_storage.clear();
+		}
+
+		template <std::convertible_to<T> Q>
+		IndexType pushBack(const Q * t, IndexType count = 1)
+		{
+			const IndexType res = _storage.size();
+			growIFN(count);
+			std::copy_n(t, count, data() + res);
+			return res;
+		}
+
+		template <std::convertible_to<T> Q>
+		IndexType pushBackMove(Q* t, IndexType count = 1)
+		{
+			const IndexType res = _storage.size();
+			growIFN(count);
+			for (IndexType i = 0; i < count; ++i)
+			{
+				data()[res + i] = std::move(t[i]);
+			}
+			return res;
+		}
+
+		constexpr const T* data() const
+		{
+			return _storage.data();
+		}
+
+		constexpr T* data()
+		{
+			return _storage.data();
+		}
+
+		constexpr IndexType size()const
+		{
+			return static_cast<IndexType>(_storage.size());
+		}
+
+		constexpr IndexType capacity()const
+		{
+			return static_cast<IndexType>(_storage.capacity());
+		}
+
+	};
+
+	template <class T, class Alloc = std::allocator<T>>
+	using ExS = ExtensibleStorage<T, Alloc>;
+}
+
+namespace std
+{
+	template<class T, class Alloc = std::allocator<T>>
+	void swap(that::ExtensibleStorage<T, Alloc>& l, that::ExtensibleStorage<T, Alloc>& r)
+	{
+		l.swap(r);
+	}
+}
