f2a69b7269b5887fe1430e2d274ab1d00e225641
Ronan <ro.cailleau@gmail.com>
2024-07-31 18:37:21 +0200

    [misc] Implemented generic enum class bit operators

---
+++ include/that/utils/EnumClassOperators.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <concepts>
+#include <bit>
+
+#define THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR(EnumClassName, EnumClassUint, op) \
+static constexpr EnumClassName& operator ## op ## =(EnumClassName& l, EnumClassName r) noexcept\
+{ \
+	EnumClassUint u = static_cast<EnumClassUint>(l); \
+	u op ## = static_cast<EnumClassUint>(r); \
+	l = EnumClassName(u); \
+	return l; \
+} \
+static constexpr EnumClassName operator ## op(EnumClassName l, EnumClassName r) noexcept\
+{ \
+	return static_cast<EnumClassName>(static_cast<EnumClassUint>(l) op static_cast<EnumClassUint>(r)); \
+}
+
+#define THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR_INTEGRAL(EnumClassName, EnumClassUint, op) \
+template <std::integral Int> \
+static constexpr EnumClassName& operator ## op ## =(EnumClassName& l, Int r) noexcept\
+{ \
+	EnumClassUint u = static_cast<EnumClassUint>(l); \
+	u op ## = static_cast<EnumClassUint>(r); \
+	l = EnumClassName(u); \
+	return l; \
+} \
+template <std::integral Int> \
+static constexpr EnumClassName operator ## op(EnumClassName l, Int r) noexcept\
+{ \
+	return static_cast<EnumClassName>(static_cast<EnumClassUint>(l) op static_cast<EnumClassUint>(r)); \
+}
+
+
+#define THAT_DECLARE_ENUM_CLASS_OPERATORS(EnumClassName, EnumClassUint) \
+THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR(EnumClassName, EnumClassUint, |) \
+THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR(EnumClassName, EnumClassUint, &) \
+THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR(EnumClassName, EnumClassUint, <<) \
+THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR(EnumClassName, EnumClassUint, >>) \
+THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR_INTEGRAL(EnumClassName, EnumClassUint, <<) \
+THAT_DECLARE_ENUM_CLASS_FLAG_OPERATOR_INTEGRAL(EnumClassName, EnumClassUint, >>)
+
