e6faf4b4c8c7c5500c1be727dc8f7241dc7328b7
Ronan Cailleau <ro.cailleau@gmail.com>
2024-11-20 17:28:45 +0100

    [misc] Added Result codes

--- include/that/core/Result.hpp
+++ include/that/core/Result.hpp
@@ -2,8 +2,9 @@
 
 namespace that
 {
-	enum class Result
-	{
+
+
+	enum class Result	{
 		Success,
 		Error,
 		Exception,
@@ -12,18 +13,36 @@ namespace that
 		NotReady,
 		Waiting,
 		InvalidValue,
+		InvalidParameter,
 		IOError,
 		FileWriteError,
 		FileReadError,
 		FileWriteException,
 		FileReadException,
+		CannotOpenFile,
+		FileDoesNotExist,
 		CannotConvertFormat,
 		STBInteralError,
 		WrongFileFormat,
+		UnknownFileExtension,
+		WrongFileMacro,
+		UnknownFileMacro,
+		UnknownMountingPoint,
+		PathIsIllFormed,
 		MAX_ENUM,
 	};
 
+	template <class T>
+	struct ResultAnd
+	{
+		// Note: this is a generic solution, it might not provide the best memory layout.
+		Result result = {};
+		T value = {};
+	};
+
+	// Returns nullptr if the result code is unknown
 	extern const char * GetResultStr(Result r);
 
+	// Never returns nullptr
 	extern const char * GetResultStrSafe(Result r);
 }
