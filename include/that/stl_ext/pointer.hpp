becf96558bd108006377d4b81b0dd3edc3508e56
Ronan Cailleau <ro.cailleau@gmail.com>
2026-04-29 21:34:22 +0200

    [misc] Improve `std::reinterpret_pointer_downcast()` to work with a templated pointer.

--- include/that/stl_ext/pointer.hpp
+++ include/that/stl_ext/pointer.hpp
@@ -21,25 +21,25 @@ namespace std
 {
 	// Given a ptr to Base which is already known to point to a Derived,
 	// Cast the shared_ptr to Derived without copying it.
-	template <class Derived, class Base>
+	template <class Derived, class Base, template <class> class _Pointer>
 		requires std::derived_from<Derived, Base>
 		// && std::is_pointer_interconvertible_base_of<Base, Derived>::value // TODO find a way to check a compile time the reinterpret cast compatibility of Base and Derived pointers
-	std::shared_ptr<Derived> const& reinterpret_pointer_downcast(std::shared_ptr<Base> const& ptr)
+	_Pointer<Derived> const& reinterpret_pointer_downcast(_Pointer<Base> const& ptr)
 	{
-		assert(dynamic_cast<const Derived*>(ptr.get()) == reinterpret_cast<const Derived*>(ptr.get())); // Check that ptr is indeed Derived
-		assert(that::CheckReinterpretCastSameAsStaticCast<Derived>(ptr.get())); // This should be doable at compile time, maybe redundant with the above check
-		return reinterpret_cast<const std::shared_ptr<Derived>&>(ptr);
+		assert(dynamic_cast<const Derived*>(std::to_address(ptr)) == reinterpret_cast<const Derived*>(std::to_address(ptr))); // Check that ptr is indeed Derived
+		assert(that::CheckReinterpretCastSameAsStaticCast<Derived>(std::to_address(ptr))); // This should be doable at compile time, maybe redundant with the above check
+		return reinterpret_cast<const _Pointer<Derived>&>(ptr);
 	}
 
 	// Given a ptr to Base which is already known to point to a Derived,
 	// Cast the shared_ptr to Derived without copying it.
-	template <class Derived, class Base>
+	template <class Derived, class Base, template <class> class _Pointer>
 		requires std::derived_from<Derived, Base>
 	// && std::is_pointer_interconvertible_base_of<Base, Derived>::value // TODO find a way to check a compile time the reinterpret cast compatibility of Base and Derived pointers
-	std::shared_ptr<Derived> & reinterpret_pointer_downcast(std::shared_ptr<Base> & ptr)
+	_Pointer<Derived> & reinterpret_pointer_downcast(_Pointer<Base> & ptr)
 	{
-		assert(dynamic_cast<const Derived*>(ptr.get()) == reinterpret_cast<const Derived*>(ptr.get())); // Check that ptr is indeed Derived
-		assert(that::CheckReinterpretCastSameAsStaticCast<Derived>(ptr.get())); // This should be doable at compile time, maybe redundant with the above check
-		return reinterpret_cast<std::shared_ptr<Derived>&>(ptr);
+		assert(dynamic_cast<const Derived*>(std::to_address(ptr)) == reinterpret_cast<const Derived*>(std::to_address(ptr))); // Check that ptr is indeed Derived
+		assert(that::CheckReinterpretCastSameAsStaticCast<Derived>(std::to_address(ptr))); // This should be doable at compile time, maybe redundant with the above check
+		return reinterpret_cast<_Pointer<Derived>&>(ptr);
 	}
 }
