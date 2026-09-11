5285eca0c2e0ec2c8c66ddb5e452fa8597571313
Ronan Cailleau <ro.cailleau@gmail.com>
2026-09-11 21:33:53 +0200

    [misc] const_forward: Remove `std::forward(const&)` override (It created a compilation issue), and "replaced" it with `std::forward_move()`

--- include/that/stl_ext/const_forward.hpp
+++ include/that/stl_ext/const_forward.hpp
@@ -4,9 +4,18 @@
 
 namespace std
 {
-	template <class T>
-	constexpr T const& forward(typename std::remove_reference<T>::type const& t)
+	// Removed it, it might break other code + It was a bad idea in the first place
+	// Instead, Rely on T const& std::move(T const&);, or std::forward_move to be explicit
+	//template <class T>
+	//constexpr T const& forward(typename std::remove_reference<T>::type const& t)
+	//{
+	//	return t;
+	//}
+
+	// Explicit forward move
+	template <class _Ty>
+	constexpr remove_reference_t<_Ty>&& forward_move(_Ty&& _Arg) noexcept
 	{
-		return t;
+		return std::move(_Arg);
 	}
 }
