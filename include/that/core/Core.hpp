cd36d04beb91d9f5422898fae034ba32efd2bed1
Ronan Cailleau <ro.cailleau@gmail.com>
2026-09-14 09:45:52 +0200

    [misc] Add `DECLARE_ENUM_VALUE_2` and `DECLARE_STRINGIFIED_2` helper macros

--- include/that/core/Core.hpp
+++ include/that/core/Core.hpp
@@ -7,11 +7,13 @@
 #define FORWARD_MACRO(X) X
 
 #define DECLARE_ENUM_VALUE_1(Name) Name,
+#define DECLARE_ENUM_VALUE_2(Name, Value) Name = Value,
 
 #define X_STRINGIFY(a) STRINGIFY(a)
 #define STRINGIFY(a) #a
 
 #define DECLARE_STRINGIFIED(X) DECLARE_ENUM_VALUE_1(STRINGIFY(X))
+#define DECLARE_STRINGIFIED_2(X, V) DECLARE_ENUM_VALUE_1(STRINGIFY(X))
 
 namespace that
 {
